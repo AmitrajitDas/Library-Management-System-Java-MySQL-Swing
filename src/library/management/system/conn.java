b3ac01f8fabab2da0e7562086ace2f50e259db43
Amitrajit31 <amitrajitdas31@gmail.com>
2021-04-28 13:21:08 +0530

    conn class

--- src/library/management/system/conn.java
+++ src/library/management/system/conn.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package library.management.system;
 
-/**
- *
- * @author Amitrajit
- */
+import java.sql.*;
+
 public class conn {
-    
+    Connection c;
+    Statement s;
+
+    public conn() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///project7", "root", "");
+            s = c.createStatement();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
